78eba1da6c748d550ac077fa4bf99e3a6ce380da
KeshiKiD03 <aaronandal03@gmail.com>
2022-04-21 13:58:20 +0200

    Aaron & Cristian Thu 21 Apr 2022 01:58:20 PM CEST

---
+++ Help Documentation/Cheats Documentation/ip-05-nat.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+# @edt ASIX M11-SAD Curs 2018-2019
+# iptables
+
+#echo 1 > /proc/sys/ipv4/ip_forward
+
+# Regles flush
+iptables -F
+iptables -X
+iptables -Z
+iptables -t nat -F
+
+# Polítiques per defecte: 
+iptables -P INPUT ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -t nat -P PREROUTING ACCEPT
+iptables -t nat -P POSTROUTING ACCEPT
+
+# obrir el localhost
+iptables -A INPUT  -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+
+# obrir la nostra ip
+iptables -A INPUT -s 192.168.0.18 -j ACCEPT
+iptables -A OUTPUT -d 192.168.0.18 -j ACCEPT
+
+# Fer NAT per les xarxes internes:
+# - 172.19.0.0/24
+# - 172.20.0.0/24
+iptables -t nat -A POSTROUTING -s 172.19.0.0/24 -o enp6s0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 172.20.0.0/24 -o enp6s0 -j MASQUERADE
